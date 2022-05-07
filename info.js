cf4f25e0f342caa790f789625cb3a2ecadd3888e
pranav-kural <pranavkural042@gmail.com>
2022-05-06 18:52:58 -0600

    documenting notes in info js

---
+++ info.js
@@ -0,0 +1,18 @@
+
+// helpers
+const clog = console.log;
+
+
+// Scroll
+
+// getting current scroll values (how much user has scrolled horizontally and vertically)
+clog('Current scroll (X: hori/Y: veri)', window.pageXOffset, window.pageYOffset);
+
+// getting coordinates of an element
+clog('Current position of cookie close btn', document.querySelector('.btn--close-cookie').getBoundingClientRect());
+
+// viewport height and width
+clog('height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth    
+);
